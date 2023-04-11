ff40b2f4e9db236373784cb8b5a355053de410aa
solenwa <solen.wanono@gmail.com>
2023-04-11 15:37:21 +0200

    MVP

--- script.js
+++ script.js
@@ -10,29 +10,42 @@ window.addEventListener('load', () => {
   canvas.style.display = 'none'
   restartBtn.style.display = 'none'
   
-  // PADDLE
+  // BACKGROUND INFO
   const bgImg = new Image()
   bgImg.src = './background.jpg'
-  const ballRadius = 20
-  const paddleWidth = 100
-  const paddleHeight = 20
-  const paddleSpeed = 6
-  let paddleX = canvas.width / 2 - paddleWidth / 2
 
-  // BALL
+  // PADDLE INFO
+  const paddleWidth = 100
+  const paddleHeight = 28
+  const paddleSpeed = 10
+  let paddleX = canvas.width / 2 - paddleWidth / 2
+  let paddleY = canvas.height - paddleHeight*2
+
+  // BALL INFO
+  const ballRadius = 15
   let ballX = paddleX + paddleWidth/ 2
-  let ballY = canvas.height - paddleHeight - ballRadius*2
+  let ballY = paddleY - ballRadius*2
   let ballSpeedX = 5
   let ballSpeedY = 5
 
   // STARTING INFO
   let isMovingLeft = false
   let isMovingRight = false
-
   let score = 0
   let gameOver = false
   let animateId
   let bricks = []
+  
+
+  // PADDLE
+  const drawPaddle = () => {
+    ctx.beginPath()
+    ctx.fillStyle = 'white'
+    // xPos, yPos, width, height
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight)
+    ctx.fill()
+    ctx.closePath()
+  }
 
   // BALL
   const drawBall = () => {
@@ -44,52 +57,38 @@ window.addEventListener('load', () => {
     ctx.closePath()
   }
 
-  // PADDLE
-  const drawPaddle = () => {
-    ctx.beginPath()
-    ctx.fillStyle = 'white'
-    // xPos, yPos, width, height
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
-    ctx.fill()
-    ctx.closePath()
-  }
-
-function randomfunction() {
-  return Math.floor(Math.random() * (3 - 1 + 1) + 1)
-}
-
   // BRICK
   class Brick {
     constructor (x, y) {
         this.x = x;
         this.y = y;
-        this.width = 35;
-        this.height = 35;
-        this.counter = 2;
+        this.width = 50;
+        this.height = 28;
+        this.counter = Math.floor(Math.random() * (4 - 1 + 1) + 1);
 
     }
 
     move() {
-        if (ballY > canvas.height - paddleHeight - ballRadius &&
+        if (ballY > paddleY - ballRadius &&
             ballX > paddleX &&
             ballX < paddleX + paddleWidth) {
-                this.y +=40
+                this.y +=28
         }
     }
 
     draw() {
       switch (this.counter){
         case 4: 
-            ctx.fillStyle = '#fea91a';
+            ctx.fillStyle = '#0096C7';
             break;
         case 3: 
-            ctx.fillStyle = '#36454f';
+            ctx.fillStyle = '#48CAE4';
             break;
         case 2:
-            ctx.fillStyle = '#7a7f80';
+            ctx.fillStyle = '#90E0EF';
             break;
         case 1:
-            ctx.fillStyle = '#a9a9a9';
+            ctx.fillStyle = '#CAF0F8';
             break;          
     }
         ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -103,27 +102,38 @@ function randomfunction() {
     checkCollision() {
         // if a ball touches a brick
         if (ballX - ballRadius < this.x + this.width &&
-            ballX > this.x &&
-            ballY - ballRadius < this.y + this.height &&
-            ballY > this.y
-        ) {
-            ballSpeedX *= -1
-            ballSpeedY *= -1
-            score += 1
-            this.counter -=1
+          ballX > this.x &&
+          ballY - ballRadius < this.y + this.height &&
+          ballY > this.y) {
+              ballSpeedX *= -1
+              ballSpeedY *= -1
+              score += 1
+              this.counter -=1
         }
+      
     }
 
   }
 
+  // Random function to draw between 1 and 3 bricks
+  function randomfunction() {
+    return Math.floor(Math.random() * (3 - 1 + 1) + 1)
+  }
+
+  // Random function to pick brick.x
+  // (10 columns possible and each column is 50 wide)
+  function randomXfunction () {
+    return Math.floor(Math.random() * (10 - 0) + 0)*50
+  }
+
   // ANIMATE FUNCTION
   const animate = () => {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height)
     drawBall()
     drawPaddle()
 
-    const bricksStillInScreen = []
+    let bricksStillInScreen = []
 
     bricks.forEach(brick => {
         brick.draw()
@@ -136,7 +146,7 @@ function randomfunction() {
             console.log('Game Over: a brick touched the floor')
         }
         // if a brick touches the paddle 
-        else if (brick.y >= canvas.height - paddleHeight - brick.height &&
+        else if (brick.y >= paddleY &&
             brick.x > paddleX &&
             brick.x < paddleX + paddleWidth) {
                 gameOver = true 
@@ -151,21 +161,31 @@ function randomfunction() {
     newArray = bricksStillInScreen.filter(brick => brick.counter !== 0)
     bricks = newArray
 
-    if (ballY > canvas.height - paddleHeight - ballRadius &&
+    // When the ball touches the paddle, create a new row of bricks + increase speed
+    if (ballY > paddleY - ballRadius &&
         ballX > paddleX &&
         ballX < paddleX + paddleWidth) {
+
+          ballSpeedX= ballSpeedX*1.03
+          ballSpeedY= ballSpeedY*1.03
+
           let randomNbr = randomfunction()
+          let randomX = randomXfunction()
+          let randomX2 = randomXfunction()
+          let randomX3 = randomXfunction()
             if(randomNbr === 1){
-              bricks.push(new Brick(Math.random() * (canvas.width-35), 0))
+              bricks.push(new Brick(randomX, 56))
             }
             else if (randomNbr === 2) {
-              brick1 = new Brick(Math.random() * (canvas.width-35), 0)
-              brick2 = new Brick(brick1.x*0.8 , 0)
+              brick1 = new Brick(randomX, 56)
+              brick2 = new Brick(randomX2, 56)
               bricks.push(brick1, brick2)
             }
             else {
-              brick1 = new Brick(Math.random() * (canvas.width-35), 0)
-              bricks.push(brick1, new Brick(300, 0), new Brick(400, 0))
+              brick1 = new Brick(randomX, 56)
+              brick2 = new Brick(randomX2, 56)
+              brick3 = new Brick(randomX3, 56)
+              bricks.push(brick1, brick2, brick3)
             }
             
     }
@@ -176,9 +196,9 @@ function randomfunction() {
       ballSpeedX *= -1
     }
     // Ball bounces off paddle
-    if (ballY > canvas.height - paddleHeight - ballRadius &&
+    if (ballY > paddleY - ballRadius &&
         ballX > paddleX &&
-        ballX < paddleX + paddleWidth + ballRadius) {
+        ballX < paddleX + paddleWidth) {
       ballSpeedY *= -1
       }
     // Ball bounces off left wall
@@ -186,7 +206,7 @@ function randomfunction() {
       ballSpeedX *= -1
     }
     // Ball bounces off ceiling
-    if (ballY < ballRadius) {
+    if (ballY < 56 + ballRadius) {
       ballSpeedY *= -1
     }
     // But if the ball touches the floor
@@ -195,23 +215,32 @@ function randomfunction() {
       console.log('Game over: the ball touched the floor')
     }
 
+    // Paddle Movement
     if (isMovingLeft && paddleX > 0) {
       paddleX -= paddleSpeed
     } else if (isMovingRight && paddleX < canvas.width - paddleWidth) {
       paddleX += paddleSpeed
     }
 
+    // Ball Movement
     ballX += ballSpeedX
     ballY += ballSpeedY
 
+    // Score Display
+    ctx.beginPath()
+    ctx.fillStyle = 'black'
+    ctx.rect(0, 0, canvas.width, 56)
+    ctx.fill()
+    ctx.fillStyle = 'white'
     ctx.font = '24px sans-serif'
-    ctx.fillText(score, 10, 30)
+    ctx.fillText(score, canvas.width / 2, 35)
+    ctx.closePath()
 
+    // Game Over
     if (gameOver) {
       cancelAnimationFrame(animateId)
-      //Display gameover text
       ctx.font = '48px sans-serif'
-      ctx.fillText('GAME OVER', canvas.width / 2 - 150, canvas.height / 2)
+      ctx.fillText('GAME OVER', canvas.width / 2 - 150, canvas.height / 3)
       restartBtn.style.display = 'block'
     } else {
       animateId = requestAnimationFrame(animate)
@@ -236,9 +265,10 @@ function randomfunction() {
     isMovingRight = false
 
     ballX = paddleX + paddleWidth/ 2
-    ballY = canvas.height - paddleHeight - ballRadius*2
+    ballY = paddleY - ballRadius*2
     ballSpeedX = 5
     ballSpeedY = 5
+    bricks = []
 
     score = 0
     gameOver = false
